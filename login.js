272c3a93135f4ff3bcda0b6cb6f0c23197c5dda1
ramajayam <ramr65007@gmail.com>
2022-09-23 14:39:00 +0400

    original file

---
+++ login.js
@@ -0,0 +1,16 @@
+function Submit(){
+    var Username=document.getElementById("Username");
+    var Password=document.getElementById("Password");
+    var error=document.querySelector("p");
+    var c=0;
+    if(Username.value=="" && Password==""){
+        alert("fhj")
+        c++;
+        console.log(c);
+        }else if(c>2){
+            alert("incorrect")
+        }
+    if(Username.value=="jagan" && Password.value=="1"){
+       window.location.assign("home.html");
+    }
+}
