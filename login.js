a140bdd01818c4b958e0afdc93d88ccf75521dc6
ramajayam <ramr65007@gmail.com>
2022-09-25 15:20:31 +0400

    Login Page

--- login.js
+++ login.js
@@ -1,16 +1,23 @@
+
+var a=0;
 function Submit(){
     var Username=document.getElementById("Username");
     var Password=document.getElementById("Password");
     var error=document.querySelector("p");
-    var c=0;
-    if(Username.value=="" && Password==""){
-        alert("fhj");
-        c++;
-        console.log(c);
-        }else if(c>2){
-            alert("incorrect")
-        }
-    if(Username.value=="jagan" && Password.value=="1"){
+    var text1=document.getElementById("text1");
+    var text2=document.getElementById("text2");
+    if(Username.value=="" && Password.value==""){
+        text1.innerHTML="UserName Is Empty";
+        text2.innerHTML="Password Is Empty";
+        }else if(Username.value=="jagan" && Password.value=="1"){
        window.location.assign("home.html");
-    }
+    }else if(Username.value!="jagan" && Password.value!="1"){
+       error.innerHTML="username and password is not Matched";
+        text1.innerHTML="";
+        text2.innerHTML="";
+        a++;
+}
+if(a>2){
+    location.replace("AccountBlock.html");
+}
 }
